06fa1e5544ae7fc2d681700b8ae8b24bee45b38b
MkaGodh <fenryrvla@yandex.ru>
2024-08-28 18:36:21 +0300

    Добавьте файлы проекта.

---
+++ structCharacterDamage/structCharacterDamage.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+
+struct character
+{
+    const char* name = "unknown";
+    int health = 0;
+    int armor = 0;
+};
+
+void take_damage(character* person, int damage)
+{
+    std::cout << person->name << " took damage: -" << damage << std::endl;
+    person->armor -= damage;
+    if (person->armor < 0)
+    {
+        person->health += person->armor;
+        person->armor = 0;
+    }
+}
+
+int main()
+{
+    character person = {"Thrall", 100};
+    person.armor = 20;
+    take_damage(&person, 30);
+    std::cout << "Health: " << person.health << " Armor:" << person.armor << std::endl;
+    
+}
+
